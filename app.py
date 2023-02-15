aa96cd3adbbc1572b18a6b32cd1bdd5665d28f6d
Nakendraprasath K <nakendraprasath321@gmail.com>
2023-02-15 21:03:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-from sklearn import metrics
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
